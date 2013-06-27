92bb3969b9e0adcb892e14830de329f151352aa7
charly <charly.simon.cs@gmail.com>
2013-06-27 19:18:53 -0400

    commit test

---
+++ test/src/Main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author aj98150
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println(System.getProperty("sun.arch.data.model"));
+
+	}
+
+}

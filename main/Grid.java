63317e9a70c670bb73794a88f16ee4dc73d476cf
igorkaimov <igorkaimov@10.192.171.174>
2013-06-27 19:45:32 -0400

    Ajout de Igor

---
+++ main/Grid.java
@@ -0,0 +1,78 @@
+package main;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Grid {
+
+	private static Map<Integer,Long> MASK;
+	
+	private long mPions = 0;
+	private long mPionsAdv = 0;
+	
+	
+	
+	
+	public Grid(String str){
+		int offset = 0;
+		for (char c : str.toCharArray()) {
+			if(c != '0')
+				mPions |= (long)1<<63-offset;
+			++offset;
+		}
+		MASK = new HashMap<Integer, Long>();
+		for(int i = 0; i < 64; ++i){
+			if(i%8 == 0){ //bord de droite
+				MASK.put(i, 1L<<i+1 | 3L <<i+7 | ((i-8 >= 0)? 3L << i-8: 0));
+			}else if((i+1)%8 == 0){// bord de gauche
+				MASK.put(i, (i+7<64?3L <<i+7:0) | 1L<<i-1 | 3L<< i-9);
+			}else{//general
+				MASK.put(i, 1L<<i+1 | (i+7<64? 7L<<i+7:0L) | 1L<<i-1 | (i-9>=0?7L<< i-9:0));
+			}
+				
+		}
+			
+		MASK.put(64, (long)1<<63 | 1 << 1);
+	}
+	
+	
+	public boolean isConnected(){
+		return isConnected(mPions);
+	}
+	
+	private boolean isConnected(long pions){
+		
+		if(pions != 0){
+			int i = 63 - Long.numberOfLeadingZeros(pions);
+			
+			
+			long nexts = 1L<<i;
+			while(nexts !=0 ){
+				
+				long comboMask = 0;
+
+				pions ^= nexts;
+				while (nexts != 0) {
+					int j = 63 - Long.numberOfLeadingZeros(nexts);
+					comboMask |= MASK.get(j);
+					nexts ^= 1L << j;
+				}
+				
+				
+				nexts = pions & comboMask;
+				
+			}
+			
+		}
+		
+		
+		
+		
+		return pions == 0;
+	}
+	
+	public void printBits(){
+		System.out.println(Long.toBinaryString(mPions));
+		
+	}
+}
---
+++ main/Main.java
@@ -0,0 +1,23 @@
+package main;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Grid grid = new Grid(
+				"10011100" +
+				"01100010" +
+				"00000010" +
+				"00000001" +
+				"00000000" +
+				"00000000" +
+				"00000000" +
+				"00000000");
+		grid.printBits();
+		System.out.println(grid.isConnected());
+
+	}
+
+}
